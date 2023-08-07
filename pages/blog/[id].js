bc775fbb7de0b0f4bae404fa5667e85382f51f86
timeisnotgood <bhoopathicl@gmail.com>
2023-08-07 21:52:04 +0530

    practice

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
 const first = () => {
+  const router = useRouter()
+  const id = router.query.id
   return (
-    <div>first</div>
+    <div>
+    <p>Dynamic Routing Page</p>
+      {id}
+    </div>
   )
 }
 
